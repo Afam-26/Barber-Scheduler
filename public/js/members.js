e851e2ac48fd5381230d22bb8977cbaff4ed8be5
Afam <afam262@gmail.com>
2021-02-01 22:00:51 -0600

    fixing travis issue

--- public/js/members.js
+++ public/js/members.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
-
-  var userID;
+  let userID;
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
